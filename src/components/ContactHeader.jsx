b0473436fe6bdea6aad0727bb88f354d3b90afff
aadarshraj02 <aadarshraazz007@gmail.com>
2024-07-18 08:38:21 +0530

    styled header

--- src/components/ContactHeader.jsx
+++ src/components/ContactHeader.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 function ContactHeader() {
   return (
-    <div>
-      <h1>Contact Us</h1>
-      <p>
+    <div className="px-24 py-5">
+      <h1 className="text-[4vw] font-bold">Contact Us</h1>
+      <p className="uppercase text-[14px] w-3/4">
         Let's connect: we are here to help, and we'd love to hear from you!
         whether you have a question,comment or just want to chat, you can reach
         out to us through the contact form of this page, or by phone,email or
