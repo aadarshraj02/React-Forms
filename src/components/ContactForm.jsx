4c0855f5335f3ef3bbdfbed5a8c9ca09cd9a15bc
aadarshraj02 <aadarshraazz007@gmail.com>
2024-07-18 08:46:08 +0530

    added image

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 
 function ContactForm() {
-  return <div></div>;
+  return (
+    <div className="flex max-w-[1000px] mx-auto">
+      <div></div>
+      <div>
+        <div>
+          <img src="../../public/contact.svg" alt="" />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default ContactForm;
