45555022b70b0740a87ff5b8c53e86340058150f
aadarshraj02 <aadarshraazz007@gmail.com>
2024-07-18 08:41:22 +0530

    imported contact Form

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Navigation from "./components/Navigation";
 import ContactHeader from "./components/ContactHeader";
+import ContactForm from "./components/ContactForm";
 
 function App() {
   return (
     <div>
       <Navigation></Navigation>
       <ContactHeader></ContactHeader>
+      <ContactForm></ContactForm>
     </div>
   );
 }
