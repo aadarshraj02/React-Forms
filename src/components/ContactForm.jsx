5ba345aa659d2255dada101b5a80484c737b21c4
aadarshraj02 <aadarshraazz007@gmail.com>
2024-07-18 11:01:33 +0530

    styled email button

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Button from "./Button";
 import { MdMessage } from "react-icons/md";
 import { IoIosCall } from "react-icons/io";
+import { IoIosMail } from "react-icons/io";
 
 function ContactForm() {
   return (
@@ -14,6 +15,9 @@ function ContactForm() {
           ></Button>
           <Button text="VIA CALL" icon={<IoIosCall />}></Button>
         </div>
+        <button className="flex items-center justify-center min-w-[350px] border-2 border-black h-10 mt-5 rounded-md">
+          VIA EMAIL <IoIosCall></IoIosCall>
+        </button>
       </div>
       <div>
         <div>
