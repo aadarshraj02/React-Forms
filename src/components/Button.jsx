2def58cf367a064c11817c2cb57673b817d30ea3
aadarshraj02 <aadarshraazz007@gmail.com>
2024-07-18 11:04:44 +0530

    updated to destructuring format

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,12 +1,11 @@
 import React from "react";
-import { MdMessage } from "react-icons/md";
-import { IoIosCall } from "react-icons/io";
 
 function Button(props) {
+  const { icon, text } = props;
   return (
     <button className="px-5 py-3 bg-black text-white rounded-lg flex items-center gap-2 justify-center">
-      {props.text}
-      {props.icon}
+      {text}
+      {icon}
     </button>
   );
 }
