9a6665f8cc70b9f0043e438d3fb53bf5792511ef
aadarshraj02 <aadarshraazz007@gmail.com>
2024-07-18 08:15:17 +0530

    added display lex on nav

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Navigation = () => {
   return (
-    <nav>
+    <nav className="flex items-center justify-between mx-auto">
       <div>
         <img src="../../public/logo.png" alt="" />
       </div>
