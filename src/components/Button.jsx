264d2f1f23f61e0a223b57a988be02ac6025d293
aadarshraj02 <aadarshraazz007@gmail.com>
2024-07-18 08:58:16 +0530

    styled button icon

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,9 +3,9 @@ import { MdMessage } from "react-icons/md";
 
 function Button() {
   return (
-    <button className="px-5 py-3 bg-black text-white rounded-sm min-w-[200px] flex items-center gap-2">
+    <button className="px-5 py-3 bg-black text-white rounded-sm min-w-[200px] flex items-center gap-2 justify-center">
       VIA SUPPORT CHAT
-      <MdMessage />
+      <MdMessage className="text-2xl" />
     </button>
   );
 }
