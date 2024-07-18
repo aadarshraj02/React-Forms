659e92c0e6693347dc1221430919861123dfed32
aadarshraj02 <aadarshraazz007@gmail.com>
2024-07-18 08:10:20 +0530

    imported navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Navigation from "./components/Navigation";
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Navigation></Navigation>
+    </div>
+  );
 }
 
 export default App;
