ff32b7247750cb349ee3b0ce0da361436d1ced73
aadarshraj02 <aadarshraazz007@gmail.com>
2024-07-18 08:12:19 +0530

    added ul and img in nav

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,7 +1,18 @@
 import React from "react";
 
 const Navigation = () => {
-  return <div></div>;
+  return (
+    <nav>
+      <div>
+        <img src="../../public/logo.png" alt="" />
+      </div>
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+  );
 };
 
 export default Navigation;
