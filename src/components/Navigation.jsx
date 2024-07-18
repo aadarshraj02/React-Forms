ecf852cf5d4bccdbef8c3ecbffbb3e11a40a7d48
aadarshraj02 <aadarshraazz007@gmail.com>
2024-07-18 08:18:32 +0530

    navigation done

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -2,11 +2,11 @@ import React from "react";
 
 const Navigation = () => {
   return (
-    <nav className="flex items-center justify-between mx-auto">
+    <nav className="flex items-center justify-between mx-auto px-10 py-5">
       <div>
         <img src="../../public/logo.png" alt="" />
       </div>
-      <ul>
+      <ul className="flex items-center gap-10 font-bold">
         <li>Home</li>
         <li>About</li>
         <li>Contact</li>
