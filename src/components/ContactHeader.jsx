50a9d150b5bff29e996be65b61b96163a5dce9f3
aadarshraj02 <aadarshraazz007@gmail.com>
2024-07-18 08:31:10 +0530

    added h1 nd a p

--- src/components/ContactHeader.jsx
+++ src/components/ContactHeader.jsx
@@ -1,7 +1,17 @@
 import React from "react";
 
 function ContactHeader() {
-  return <div></div>;
+  return (
+    <div>
+      <h1>Contact Us</h1>
+      <p>
+        Let's connect: we are here to help, and we'd love to hear from you!
+        whether you have a question,comment or just want to chat, you can reach
+        out to us through the contact form of this page, or by phone,email or
+        social media.
+      </p>
+    </div>
+  );
 }
 
 export default ContactHeader;
