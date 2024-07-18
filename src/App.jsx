808bfae83bbe57f9fb57e6534598c01ae25115c3
aadarshraj02 <aadarshraazz007@gmail.com>
2024-07-18 08:28:32 +0530

    imported contact header

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Navigation from "./components/Navigation";
+import ContactHeader from "./components/ContactHeader";
 
 function App() {
   return (
     <div>
       <Navigation></Navigation>
+      <ContactHeader></ContactHeader>
     </div>
   );
 }
