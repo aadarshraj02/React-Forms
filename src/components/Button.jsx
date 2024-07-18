c81609b4423a76615700f88e23704daca12ff1f3
aadarshraj02 <aadarshraazz007@gmail.com>
2024-07-18 08:56:16 +0530

    added a button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,13 @@
 import React from "react";
+import { MdMessage } from "react-icons/md";
 
 function Button() {
-  return <div></div>;
+  return (
+    <button className="px-5 py-3 bg-black text-white rounded-sm min-w-[200px] flex items-center gap-2">
+      VIA SUPPORT CHAT
+      <MdMessage />
+    </button>
+  );
 }
 
 export default Button;
